7c021f23e4e3a0aba0549d273675797f59da0713
Alex <64538834+alex-safonov@users.noreply.github.com>
2023-03-28 11:38:02 +0300

    Create tests.php

---
+++ tests.php
@@ -0,0 +1,25 @@
+print_r(((string)3.04)." times");
+============================================================
+
+$name = 'anneirB';
+============================================================
+
+print_r($eurosCount * 1.25);
+print_r("\n");
+print_r($eurosCount * 1.25 * 60);
+============================================================
+
+namespace App\Solution;
+$stark = 'Arya';
+// BEGIN (write your solution here)
+print_r("Do you want to eat, {$stark}?");
+// END
+============================================================
+
+$one = "Naharis";
+$two = "Mormont";
+$three = "Sand";
+// BEGIN (write your solution here)
+print_r($one[2].$two[1].$three[3].substr($two,1,2));
+// END
+============================================================
