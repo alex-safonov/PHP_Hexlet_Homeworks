a880e2ef3725960c651713db62ad7eed486643cc
Alex <64538834+alex-safonov@users.noreply.github.com>
2023-04-03 17:53:18 +0300

    Update tests.php

--- tests.php
+++ tests.php
@@ -400,4 +400,21 @@ function isPalindrome($string)
 }
 // END
 
+          
+namespace Strings;
+
+function reverse($str)
+{
+    $index = strlen($str) - 1;
+    $reversedString = '';
+
+    while ($index >= 0) {
+        $currentChar = $str[$index];
+        $reversedString = "{$reversedString}{$currentChar}";
+        $index = $index - 1;
+    }
+
+    return $reversedString;
+}
+
 ============================================================
